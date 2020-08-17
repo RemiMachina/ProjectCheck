c8a0a3a7ebe6743a9765f2ac38bf03c0162ead3a
quoimec-laptop <charlieschacher@gmail.com>
2020-08-17 18:31:16 +1000

    PR-921: Slighly tweaked output names

--- source/pylint/run.py
+++ source/pylint/run.py
@@ -118,7 +118,7 @@ for file, data in processed.items():
                 
 
 # Output to Github  
-subprocess.run(f"echo \"::set-output name=pylint_errors::{total_errors}\"", shell = True) 
-subprocess.run(f"echo \"::set-output name=pylint_warnings::{total_warnings}\"", shell = True) 
-subprocess.run(f"echo \"::set-output name=pylint_suggestions::{total_suggestions}\"", shell = True) 
+subprocess.run(f"echo \"::set-output name=errors::{total_errors}\"", shell = True) 
+subprocess.run(f"echo \"::set-output name=warnings::{total_warnings}\"", shell = True) 
+subprocess.run(f"echo \"::set-output name=suggestions::{total_suggestions}\"", shell = True) 
 
