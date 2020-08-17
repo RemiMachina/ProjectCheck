fe04ec77ce51098c73f8825f6c56a83b6b96cfd9
quoimec-laptop <charlieschacher@gmail.com>
2020-08-18 09:44:09 +1000

    PR-921: Testing new method of outputting data

--- source/pylint/run.py
+++ source/pylint/run.py
@@ -115,14 +115,10 @@ for file, data in processed.items():
                 print(line.format(line_indent, column_indent, id_indent))
             else:
                 print(line.format(newline_indent))
-                
-print(total_errors)
-print(total_warnings)
-print(total_suggestions)
-
+        
 
 # Output to Github  
-subprocess.run(f"echo \"::set-output name=pylint_errors::{total_errors}\"", shell = True) 
-subprocess.run(f"echo \"::set-output name=pylint_warnings::{total_warnings}\"", shell = True) 
-subprocess.run(f"echo \"::set-output name=pylint_suggestions::{total_suggestions}\"", shell = True) 
+print(f"::set-output name=pylint_errors::{total_errors}")
+print(f"::set-output name=pylint_warnings::{total_warnings}")
+print(f"::set-output name=pylint_suggestions::{total_suggestions}")
 
