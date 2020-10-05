08a8f631b0e5e9ed6deb334c4faf3cec2f8b596d
quoimec-laptop <charlieschacher@gmail.com>
2020-10-06 10:13:28 +1100

    PR-921: Fixed typo

--- source/common/git.py
+++ source/common/git.py
@@ -55,7 +55,7 @@ class GitIssue:
         }
             
         try:
-            common_warning = ">**Note:**\r\n>" + common_issues[first.message_id] = "\r\n"
+            common_warning = ">**Note:**\r\n>" + common_issues["E0611"] + "\r\n"
         except KeyError:
             common_warning = ""
             
